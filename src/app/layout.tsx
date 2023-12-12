8e5fbe2a1a5830d9d7c692cb1783af513c55ff8e
Bogdan Safronov <bogdasafronov@gmail.com>
2023-12-12 12:54:39 +0300

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "~/styles/globals.css";
+
+import { Inter } from "next/font/google";
+import { cookies } from "next/headers";
+
+import { TRPCReactProvider } from "~/trpc/react";
+import { Toaster } from "~/components/ui/toaster";
+
+const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Helium",
+  description: "Helium by Safronov",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body className={`font-sans ${inter.variable}`}>
+        <TRPCReactProvider cookies={cookies().toString()}>
+          <Toaster />
+          {children}
+        </TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
