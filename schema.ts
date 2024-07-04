2fb2e75fe086dee83a628a1c18b939a497e10166
Bogdan Safronov <bogdasafronov@gmail.com>
2024-07-04 12:43:06 +0300

    add db auth models

--- schema.ts
+++ schema.ts
@@ -1,6 +1,45 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const id = serial("id").primaryKey();
+export const createdAt = timestamp("created_at", { withTimezone: true })
+  .notNull()
+  .defaultNow();
+export const userId = integer("user_id")
+  .notNull()
+  .references(() => usersTable.id);
+
+export const usersTable = pgTable("users", {
+  id,
+  createdAt,
+  username: varchar("username", { length: 256 })
+    .notNull()
+    .$defaultFn(() => sql`uuid_generate_v4()`),
+  hashPassword: varchar("hash_password", { length: 256 }).notNull(),
+});
+
+export const usersRelations = relations(usersTable, ({ many, one }) => ({
+  sessions: many(sessionsTable),
+}));
+
+export const sessionsTable = pgTable("sessions", {
+  id: text("id").primaryKey(),
+  userId,
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
 
 export const postsTable = pgTable("posts", {
-  id: serial("id").primaryKey(),
+  id,
+  createdAt,
   text: text("text"),
 });
